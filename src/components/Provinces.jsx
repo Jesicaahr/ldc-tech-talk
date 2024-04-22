2393c86dbb7d78ea4e009a319de1ba37e6ca7f3b
jesicaahr <jesicaprilia18@gmail.com>
2024-04-22 08:27:10 +0700

    change regency style

--- src/components/Provinces.jsx
+++ src/components/Provinces.jsx
@@ -15,7 +15,6 @@ const Provinces = () => {
         .catch((err) => console.log(err));
     },
   });
-
   const { data: regencies, isLoading: isLoadingRegencies } = useQuery({
     queryKey: ["REGENCIES", provinceId],
     queryFn: async () => {
@@ -27,8 +26,8 @@ const Provinces = () => {
     },
     enabled: Boolean(provinceId),
   });
-  console.log(provinceId);
-  console.log(regencies);
+  console.log("PROVINCE ID: ", provinceId);
+  console.log("DATA REGENCY: ", regencies);
 
   if (isLoadingProvinces) return <p>DATA PROVINCES LOADING . . . </p>;
   if (!provinces) return <p>DATA PROVINCE NOT FOUND</p>;
@@ -36,16 +35,12 @@ const Provinces = () => {
   function renderRegencies() {
     if (isLoadingRegencies) return <p>DATA REGENCY LOADING</p>;
     if (!regencies) return <p>DATA REGENCY FOR {provinceName} NOT FOUND</p>;
-
     return regencies.map((regency) => (
-      <li key={regency.id}>
-        <button className="mb-2 rounded-md bg-emerald-600 px-3 py-1 text-white hover:bg-emerald-500">
-          {regency.name}
-        </button>
+      <li key={regency.id} className="mb-2 rounded-md py-1 text-sm ">
+        {regency.name}
       </li>
     ));
   }
-
   return (
     <>
       <div className="flex gap-x-12">
@@ -68,7 +63,9 @@ const Provinces = () => {
           </ul>
         </div>
         <div>
-          <h1 className="mb-2 mt-3 text-lg font-medium">REGENCY</h1>
+          <h1 className="mb-2 mt-3 text-lg font-medium">
+            {provinceName} REGENCY
+          </h1>
           <ul>{renderRegencies()}</ul>
         </div>
       </div>
