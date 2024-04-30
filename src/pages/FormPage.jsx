d6e93db01acd02abc9ca62d17ecce1be383aafec
jesicaahr <jesicaprilia18@gmail.com>
2024-04-30 17:38:53 +0700

    add form

--- src/pages/FormPage.jsx
+++ src/pages/FormPage.jsx
@@ -1,7 +1,98 @@
-import React from "react";
+import { useForm } from "react-hook-form";
+import { ErrorMessage } from "@hookform/error-message";
 
 const FormPage = () => {
-  return <div>FormPage</div>;
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+    },
+  });
+  const onSubmit = (data) => {
+    console.log("MASUK ON SUBMIT");
+    if (!data) {
+      console.error("Data is null or undefined");
+      return;
+    }
+
+    try {
+      console.log(data);
+      reset();
+    } catch (error) {
+      console.error("An error occurred during form submission:", error);
+    }
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="mb-4">
+          <label
+            htmlFor="firstName"
+            className="mb-2 block font-bold text-gray-700"
+          >
+            First Name
+          </label>
+          <input
+            type="text"
+            id="firstName"
+            placeholder="Enter your first name"
+            className="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
+            {...register("firstName", {
+              required: "Please input your first name",
+            })}
+          />
+
+          <ErrorMessage
+            errors={errors}
+            name="firstName"
+            render={({ message }) => (
+              <p className="ml-3 text-red-500">{message}</p>
+            )}
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="lastName"
+            className="mb-2 block font-bold text-gray-700"
+          >
+            Last Name
+          </label>
+          <input
+            type="text"
+            id="lastName"
+            placeholder="Enter your last name"
+            className="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
+            {...register("lastName")}
+          />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="email" className="mb-2 block font-bold text-gray-700">
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            placeholder="Enter your email"
+            className="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
+            {...register("email")}
+          />
+        </div>
+        <button
+          type="submit"
+          className="focus:shadow-outline rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700 focus:outline-none"
+        >
+          Submit
+        </button>
+      </form>
+    </>
+  );
 };
 
 export default FormPage;
